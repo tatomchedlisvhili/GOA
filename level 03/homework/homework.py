503d25420f22589b2c2e6e939749ea8e93d16042
tatomchedlisvhili <tatomchedlishvili3@gmail.com>
2024-12-23 00:12:36 +0400

    GOA

---
+++ level 03/homework/homework.py
@@ -0,0 +1,31 @@
+age = 17
+age = 22
+
+print(age)
+
+
+mom = 48   
+dad = 46 
+sister = 23
+grandmother = 68 
+
+print( mom + dad + sister + grandmother)
+
+
+
+name = "tato"
+
+name = "nika"
+
+print(name)
+
+
+number = 5-1
+number2 = 5+1
+number3 = 10/2
+number4 = 5*2
+
+print(number)
+print(number2)
+print(number3)
+print(number4)
---
+++ level 06/homework/homework.py
@@ -0,0 +1,70 @@
+
+#1
+
+num = int(input("chaweret tkveni asaki: "))
+
+num2 = num + 10
+
+print  (num2)
+
+num3= float(num2)
+
+print (num3)
+
+print (type(num3))
+
+
+
+#2
+
+surname= input(" chaweret tkveni gvari: ")
+
+print (surname * 100)
+
+
+
+
+
+
+#3
+
+n1= "sax"
+n2= "li"
+n3 = "da"
+n4 = "cha"
+n5 = "va"
+n6 = "shli"
+
+print (n1 + n2)
+print (n3 + n4)
+print (n5 + n6)
+
+#4
+
+# სტრინგს ვიყენებთ იმისთვის რომ გამოვსახოთ ტექსტური ტიპის ინფორმაცია
+# ინტეჯერს ვიყენებთ მთელი რიცხვებისთვის 
+# floats ვიყენებთ წილადი რიცხვებისთვის
+# boolean-ს ვიყენებთ იმისთვის რომ გამოვსახოთ რაიმე იმფორმაციის სიმართლე  ან ტყუილი მაგ: love_pets = True
+
+
+
+#5
+
+name = "tato"
+age = 17
+decimal = 10.2
+love_basketball= True
+
+
+print(type(name))
+print(type(age))
+print(type(decimal))
+print(type(love_basketball))
+
+
+
+
+
+
+
+
